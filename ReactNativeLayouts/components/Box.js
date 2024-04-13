5b655bb1d1fdee22b50de8c2347743e9db5c0ce6
Dhiraj <dhiraj.basavaraju@gmail.com>
2024-04-14 01:51:00 +0530

    more about layouts

---
+++ ReactNativeLayouts/components/Box.js
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const Box = ({ children, style }) => {
+  return (
+    <View style={[styles.box, style]}>
+      <Text style={styles.text}>{children}</Text>
+    </View>
+  );
+};
+
+export default Box;
+
+const styles = StyleSheet.create({
+  box: {
+    backgroundColor: "white",
+    padding: 30,
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "600",
+    textAlign: "center",
+    color: "white",
+  },
+});
